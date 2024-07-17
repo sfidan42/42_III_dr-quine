2695e8448e843cdc28a1cf4087ec8ffe07828ed4
sfidan42 <sadettinfidan00@gmail.com>
2024-07-17 15:07:43 +0300

    colleen asm done

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -1,21 +1,19 @@
-        extern	printf		; the C function, to be called
+; Colleen ASM
+section .text
+	global main
+	extern printf
+main:
+	push rbp
+	; printf args: rdi, rsi, rdx, rcx, r8d
+	mov r8d, fmt
+	mov rcx, 59
+	mov rdx, 34
+	mov rsi, 10
+	mov rdi, fmt
+	call printf
+	pop rbp
+	mov rax, 0
+	ret
 
-        section .data		; Data section, initialized variables
-msg:	db "Hello world", 0	; C string needs 0
-fmt:    db "%s", 10, 0          ; The printf format, "\n",'0'
-
-        section .text           ; Code section.
-
-        global main		; the standard gcc entry point
-main:				; the program label for the entry point
-        push    rbp		; set up stack frame, must be aligned
-	
-	mov	rdi,fmt
-	mov	rsi,msg
-	mov	rax,0		; or can be  xor  rax,rax
-        call    printf		; Call C function
-
-	pop	rbp		; restore stack
-
-	mov	rax,0		; normal, no error, return value
-	ret			; return
+section .data
+	fmt: db "%3$c Colleen ASM%1$csection .text%1$c	global main%1$c	extern printf%1$cmain:%1$c	push rbp%1$c	%3$c printf args: rdi, rsi, rdx, rcx, r8d%1$c	mov r8d, fmt%1$c	mov rcx, 59%1$c	mov rdx, 34%1$c	mov rsi, 10%1$c	mov rdi, fmt%1$c	call printf%1$c	pop rbp%1$c	mov rax, 0%1$c	ret%1$c%1$csection .data%1$c	fmt: db %2$c%4$s%2$c, 0%1$c", 0
